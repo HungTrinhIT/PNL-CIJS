f84a35943b428c72e152f50536e27f9f0959af97
HungTrinhJH <hung.trinh@journeyh.io>
2022-04-21 15:28:06 +0700

    Update

---
+++ Lesson-02/demo-react/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./Header";
+import FooterClassComponent from "./FooterClassComponent";
+
+function App() {
+  return <div className="App">
+    <h1>Demo Props and State</h1>
+  </div>;
+}
+
+export default App;
+
+/*
+  React co 2 loai component:
+    - Functional component/ Stateless component
+    - Class Component/ Statefull component
+
+    Return ve 1 JSX (Javascription XML ~ HTML: viet JS trong HTML)
+    Conditional Rendering
+
+  Review ES6
+ */
---
+++ Lesson-02/demo-react/src/FooterClassComponent.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class FooterClassComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Day la footer nha</h1>
+        <h2>Ban quyen cam sao chep</h2>
+      </div>
+    );
+  }
+}
+
+export default FooterClassComponent;
